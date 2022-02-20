b0335cb755211b10594923076cb1b1ee6ccc774b
wuksow <nikodemrutyna1@gmail.com>
2022-02-20 14:55:15 +0100

    Added command /chat clear

--- src/main/java/rcode/chat/Main.java
+++ src/main/java/rcode/chat/Main.java
@@ -1,8 +1,12 @@
 package rcode.chat;
 
+import com.google.common.collect.Sets;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import rcode.chat.cmd.ChatCMD;
 
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,11 +14,15 @@ public class Main extends JavaPlugin {
 
     private final Logger wiad = Bukkit.getLogger();
 
+    public static Set<Integer> chat = Sets.newConcurrentHashSet();
+
     @Override
     public void onEnable() {
         super.onEnable();
+        saveDefaultConfig();
         wiad.log(Level.INFO, "Enable plugin for Chat!");
         wiad.log(Level.INFO, "RCode plugin!");
+        getCommand("chat").setExecutor(new ChatCMD());
     }
 
     @Override
--- src/main/java/rcode/chat/cmd/ChatCMD.java
+++ src/main/java/rcode/chat/cmd/ChatCMD.java
@@ -1,13 +1,57 @@
 package rcode.chat.cmd;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import rcode.chat.Main;
 
 public class ChatCMD implements CommandExecutor {
+
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String s, String[] strings) {
+    public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
         boolean zwrot = true;
+        Player p = (Player)sender;
+        if (!p.hasPermission(Main.getInstance().getConfig().getString("permission"))) {
+            String msg = Main.getInstance().getConfig().getString("nopermMSG");
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+            return false;
+        }
+        String clearp, clear;
+        clearp = Main.getInstance().getConfig().getString("succesClearChatMessageToExecutor");
+        clear = Main.getInstance().getConfig().getString("succesClearChatMessageToPlayers");
+        switch (args[0].toLowerCase()) {
+            case "clear":
+            case "cc":
+                Bukkit.getOnlinePlayers().forEach(player -> {
+                    player.sendMessage(new String[100]);
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', clear));
+                });
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', clearp));
+                break;
+            case "off":
+                if (Main.chat.equals(0)) {
+                    return false;
+                }
+                Main.chat.clear();
+                Main.chat.add(0);
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', ""));
+                break;
+            case "on":
+                if (Main.chat.equals(1)) {
+                    return false;
+                }
+                Main.chat.clear();
+                Main.chat.add(1);
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', ""));
+                break;
+            default:
+                String usage = Main.getInstance().getConfig().getString("usage");
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', usage));
+                break;
+        }
         return zwrot;
     }
 }
